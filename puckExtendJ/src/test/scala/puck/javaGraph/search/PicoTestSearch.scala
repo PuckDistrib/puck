a58f85a32c2af746e5af7504d108d0d9aa8e4265
puckdistrib <cedric.besse@gmail.com>
2017-10-06 14:37:13 +0200

    change control in PicoTestSearch

--- puckExtendJ/src/test/scala/puck/javaGraph/search/PicoTestSearch.scala
+++ puckExtendJ/src/test/scala/puck/javaGraph/search/PicoTestSearch.scala
@@ -44,10 +44,10 @@ object PicoTestSearch {
       10, 100, solsDir)
 
 
-  //  val control = new ControlWithHeuristic(Rules, scenario.graph.newGraph(mutabilitySet = scenario.initialMutability),
-   //  constraints, NoVirtualNodes, violationsKindPriority).asInstanceOf[SearchControl[DecoratedGraph[Any]]]
-   val control = new BlindControl(Rules, scenario.graph.newGraph(mutabilitySet = scenario.initialMutability),
-      constraints, NoVirtualNodes, violationsKindPriority).asInstanceOf[SearchControl[DecoratedGraph[Any]]]
+    val control = new ControlWithHeuristic(Rules, scenario.graph.newGraph(mutabilitySet = scenario.initialMutability),
+     constraints, NoVirtualNodes, violationsKindPriority).asInstanceOf[SearchControl[DecoratedGraph[Any]]]
+   //val control = new BlindControl(Rules, scenario.graph.newGraph(mutabilitySet = scenario.initialMutability),
+   //   constraints, NoVirtualNodes, violationsKindPriority).asInstanceOf[SearchControl[DecoratedGraph[Any]]]
 
     // SearchEngine(strategy, control, Some(1)) :  Some(n) => n sol(s), None => all sols
     val engine = new SearchEngineWithLoggedFitness(strategy, control, constraints, None, Some(evaluator))
